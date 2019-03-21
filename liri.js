71211e71afb41b66b5faf36449db8dc833efaa46
Christopher Warner <christopher.warner92@gmail.com>
2019-03-21 18:34:01 -0400

    added switch command to execute input command

--- liri.js
+++ liri.js
@@ -3,4 +3,34 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var inputCommand = process.argv[2];
-console.log(inputCommand);
+
+function concert_this() {
+    console.log("concert_this() executed")
+}
+
+function spotify_this_song() {
+    console.log("spotify_this_song() executed");
+}
+
+function movie_this() {
+    console.log("movie_this() executed");
+}
+
+function do_what_it_says() {
+    console.log("do_what_it_says() executed");
+}
+
+switch(inputCommand) {
+    case "concert-this":
+    concert_this();
+    break;
+    case "spotify-this-song":
+    spotify_this_song();
+    break;
+    case "movie-this":
+    movie_this();
+    break;
+    case "do-what-it-says":
+    do_what_it_says();
+    break;
+}
