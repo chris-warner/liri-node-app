b09447f80147f2e17d58dae96b2b6e7c96966e39
Christopher Warner <christopher.warner92@gmail.com>
2019-03-21 19:12:28 -0400

    Finished concert-this intergration

--- liri.js
+++ liri.js
@@ -2,11 +2,30 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var axios = require("axios");
+var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
 var inputCommand = process.argv[2];
+var inputCommand2 = process.argv[3];
 
 function concert_this() {
-    console.log("concert_this() executed")
+    console.log("concert_this() executed");
+    var artist = inputCommand2;
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+    function(response) {
+    for(var i = 0; i < response.data.length; i++) {
+    var show = {
+        name: response.data[i].venue.name,
+        location: response.data[i].venue.city + ", " + response.data[i].venue.country,
+        date: moment(response.data[i].datetime).format('MM-DD-YYYY')
+    };
+    console.log("("+(i+1)+")" + "------------------------------------------------------------------");
+    console.log("Name of Venue: " + show.name);
+    console.log("Location: " + show.location);
+    console.log("Date of Event: " + show.date);
+    console.log("---------------------------------------------------------------------");
+    }
+  }
+);
 }
 
 function spotify_this_song() {
